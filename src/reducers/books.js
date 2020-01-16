1da7fc06a3edbfcc7771ef92484f68d917f46c5d
juzQrios <dj30c.1@gmail.com>
2020-01-16 17:09:21 +0530

    Fix books redcuer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -10,21 +10,15 @@ const initialState = {
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'CREATE_BOOK': return {
+    case 'CREATE_BOOK': return [
       ...state,
-      books: [
-        ...state.books,
-        {
-          id: Math.random(),
-          title: action.title,
-          category: action.category,
-        },
-      ],
-    };
-    case 'REMOVE_BOOK': return {
-      ...state,
-      books: state.books.filter(book => book.id !== action.id),
-    };
+      {
+        id: Math.random(),
+        title: action.title,
+        category: action.category,
+      }
+    ];
+    case 'REMOVE_BOOK': return state.books.filter(book => book.id !== action.id);
     default: return state;
   }
 };
