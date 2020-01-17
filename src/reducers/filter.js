07639429c2ce1d94e30d733b70bbbc8031d00ca9
juzQrios <dj30c.1@gmail.com>
2020-01-17 21:49:07 +0530

    Add 'filter' key to the application's Redux store

---
+++ src/reducers/filter.js
@@ -0,0 +1,12 @@
+const initialState = 'All';
+
+const filterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CHANGE_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
