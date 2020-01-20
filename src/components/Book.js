904a733941ab4facdf2866f2220310f5bc8f8a9b
addo <forison6205@gmail.com>
2020-01-20 07:57:35 -0500

    added: styling to the main body of page

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 
 function Book({ book, clickDelete }) {
   return (
-    <tr>
-      <th scope="col">{ book.id }</th>
-      <th scope="col">{ book.title }</th>
-      <th scope="col">{book.category}</th>
-      <th scope="col"><button type="button" className="btn btn-danger" onClick={() => clickDelete(book.id)}>delete</button></th>
-    </tr>
+    <div className="card p-2 m-3">
+      <div className="card-body d-flex justify-content-between align-items-center">
+        <div>
+          <h4>{book.category}</h4>
+          <h2>{book.title}</h2>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="btn mainColor"
+            onClick={() => clickDelete(book.id)}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 Book.propTypes = {
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -22,26 +22,16 @@ function BooksList({
   const visible = getVisibleBooks(books, filter);
   return (
     <div className="container BooksList">
-      <div className="mx-auto col-9 shadow-lg mt-3">
-        <div className="p-4">
+      <div className="mx-auto col-9 shadow-lg mt-3 ">
+        <div className="p-3">
           <Header onFilterChange={handleFilterChange} />
-        </div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Book-id</th>
-              <th scope="col">Title</th>
-              <th scope="col">Category</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
+        </div >
             {
               visible.map(book => <Book clickDelete={handleRemoveBook} key={book.id} book={book} />)
-            }
-          </tbody>
-        </table>
+        }
+        <div className="clearfix"></div>
       </div>
+      
     </div>
   );
 }
